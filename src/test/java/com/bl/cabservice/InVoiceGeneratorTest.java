3a7ecd5af378987f8ae5c1529b9e5e3278a9078b
Manasi2804 <shahmanasi281996@gmail.com>
2020-05-13 15:14:50 +0530

    ADDED:Test case for to return minimum fare

--- src/test/java/com/bl/cabservice/InVoiceGeneratorTest.java
+++ src/test/java/com/bl/cabservice/InVoiceGeneratorTest.java
@@ -13,4 +13,11 @@ public class InVoiceGeneratorTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(40, fare, 0);
     }
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+        double distance = 0.1;
+        int time = 2;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0);
+    }
 }
