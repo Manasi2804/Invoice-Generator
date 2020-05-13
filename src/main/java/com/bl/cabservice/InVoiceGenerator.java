2d40fd1c04d1113435937610a8b5943d3b61d77a
Manasi2804 <shahmanasi281996@gmail.com>
2020-05-13 15:01:17 +0530

    ADDED:To calculate total fare

--- src/main/java/com/bl/cabservice/InVoiceGenerator.java
+++ src/main/java/com/bl/cabservice/InVoiceGenerator.java
@@ -1,7 +1,9 @@
 package com.bl.cabservice;
 
 public class InVoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome to cab invoice problem");
+    private static final double RATE_PER_KM = 10;
+    private static final int RATE_PER_MIN = 1;
+    public double calculateFare(double distance, int time) {
+        return RATE_PER_KM * distance + RATE_PER_MIN * time;
     }
 }
