618bb00ba30559a7e306775d6834ca4210f4d28f
Manasi2804 <shahmanasi281996@gmail.com>
2020-05-14 08:30:27 +0530

    ADDED:To obtain list of rides by userID from RideRepository

--- src/main/java/com/bl/cabservice/InVoiceGenerator.java
+++ src/main/java/com/bl/cabservice/InVoiceGenerator.java
@@ -1,9 +1,13 @@
 package com.bl.cabservice;
 
+import java.util.ArrayList;
+
 public class InVoiceGenerator {
     private static final double RATE_PER_KM = 10;
     private static final int RATE_PER_MIN = 1;
     private static final double MINIMUM_FARE = 5.0;
+    RideRepository rideRepository = new RideRepository();
+    ArrayList<Ride> listOfRides = new ArrayList<Ride>();
 
     public double calculateFare(double distance, int time) {
         double totalFare = RATE_PER_KM * distance + RATE_PER_MIN * time;
@@ -12,11 +16,19 @@ public class InVoiceGenerator {
     public double calculateFareForMultipleRides(Ride[] rides) {
         double aggregateFare = 0;
         for (Ride ride : rides) {
+            listOfRides.add(ride);
             aggregateFare = aggregateFare + calculateFare(ride.distance, ride.time);
         }
         return aggregateFare;
     }
     public InvoiceSummary getInvoiceSummary(Ride[] rides) {
         return new InvoiceSummary(rides.length, calculateFareForMultipleRides(rides));
+    }
+    public void addRides(String userId) {
+        rideRepository.addUserRides(userId, listOfRides);
+    }
+    public ArrayList<Ride> getRidesByUserId(String userId) {
+        ArrayList<Ride> ridesByUserId = rideRepository.getRidesByUserId(userId);
+        return ridesByUserId;
     }
 }
--- src/test/java/com/bl/cabservice/InVoiceGeneratorTest.java
+++ src/test/java/com/bl/cabservice/InVoiceGeneratorTest.java
@@ -21,17 +21,30 @@ public class InVoiceGeneratorTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0);
     }
+
     @Test
     public void givenMultipleRideDetails_ShouldReturnTotalFare() {
         Ride[] rides = {new Ride(4.0, 15), new Ride(3.0, 11), new Ride(6.0, 25)};
         double totalFare = invoiceGenerator.calculateFareForMultipleRides(rides);
         Assert.assertEquals(181, totalFare, 0);
     }
+
     @Test
     public void givenMultipleRideDetails_ShouldReturnInvoiceSummary() {
         Ride[] rides = {new Ride(4.0, 15), new Ride(3.0, 11), new Ride(6.0, 25)};
         InvoiceSummary invoiceSummary = invoiceGenerator.getInvoiceSummary(rides);
-        InvoiceSummary expectedSummary = new InvoiceSummary(3,181.0);
+        InvoiceSummary expectedSummary = new InvoiceSummary(3, 181.0);
         Assert.assertEquals(expectedSummary, invoiceSummary);
+    }
+
+    @Test
+    public void givenUserId_ShouldReturnListOfRidesAndInvoice() {
+        String userId = "a@b.com";
+        Ride[] rides = {new Ride(4.0, 15), new Ride(3.0, 11), new Ride(6.0, 25)};
+        invoiceGenerator.addRides(userId);
+        InvoiceSummary invoiceSummary = invoiceGenerator.getInvoiceSummary(rides);
+        InvoiceSummary expectedSummary = new InvoiceSummary(3, 181.0);
+        Assert.assertEquals(expectedSummary, invoiceSummary);
+        Assert.assertEquals(rides.length, invoiceGenerator.getRidesByUserId(userId).size());
     }
 }
